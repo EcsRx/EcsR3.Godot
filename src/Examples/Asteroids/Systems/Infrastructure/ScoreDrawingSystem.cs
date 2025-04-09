a5b7ea10c6bc10ac520c00a30f66e1684071050a
LP <grofit1@yahoo.com>
2025-04-09 14:41:33 +0100

    Updated more functionality based off the MG version

---
+++ src/Examples/Asteroids/Systems/Infrastructure/ScoreDrawingSystem.cs
@@ -0,0 +1,32 @@
+using EcsR3.Entities;
+using EcsR3.Extensions;
+using EcsR3.Godot.Examples.Asteroids.Components;
+using EcsR3.Godot.Examples.Asteroids.Extensions;
+using EcsR3.Groups;
+using EcsR3.Systems;
+using Godot;
+using SystemsR3.Attributes;
+using SystemsR3.Scheduling;
+using SystemsR3.Types;
+
+namespace EcsR3.Godot.Examples.Asteroids.Systems.Infrastructure;
+/*
+[Priority(PriorityTypes.SuperLow)]
+public class ScoreDrawingSystem : IBasicEntitySystem
+{
+    public IGroup Group { get; } =  new Group(typeof(PlayerComponent));
+    public Label ScoreLabel { get; set; } 
+        
+    public ScoreDrawingSystem()
+    {
+       // this.RunOnGodotThread(() => ScoreLabel = this.GetRootScene().CurrentScene.FindChild("ScoreValue") as Label);
+    }
+
+    public void Process(IEntity entity, ElapsedTime elapsedTime)
+    {
+        if(ScoreLabel == null) { return; }
+        
+        var playerComponent = entity.GetComponent<PlayerComponent>();
+        ScoreLabel.Text = playerComponent.Score.ToString();
+    }
+}*/
